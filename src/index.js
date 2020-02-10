16b7478d9e00b4ff5ee06ae53add0df612260a4d
Titouan Mathis <titouan@studiometa.fr>
2020-02-10 15:05:58 +0100

    Delete the typography configuration
    It is the same as the default one.
    See https://tailwindcss.com/docs/font-weight

--- src/index.js
+++ src/index.js
@@ -100,21 +100,6 @@ module.exports = {
     order: {},
 
     /**
-     * Typography
-     */
-    fontWeight: {
-      hairline: '100',
-      thin: '200',
-      light: '300',
-      normal: '400',
-      medium: '500',
-      semibold: '600',
-      bold: '700',
-      extrabold: '800',
-      black: '900',
-    },
-
-    /**
      * Size
      */
     width: theme => ({
