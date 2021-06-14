df2420b072fff1a9434c24fa4d7ae9bbb60618a4
Titouan Mathis <titouan@studiometa.fr>
2021-06-14 19:09:02 +0200

    Update doc to VuePress 2

---
+++ packages/tests/tailwind.config.js
@@ -0,0 +1,5 @@
+module.exports = require('@studiometa/tailwind-config');
+
+module.exports.purge = { content: ['index.html'] };
+
+// module.exports.mode = 'jit';
--- tailwind.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = require('./src/index.js');
-
-module.exports.purge = { content: ['./tests/**/*.html'] };
-module.exports.theme.debugOutline = true;
-
-module.exports.mode = 'jit';
