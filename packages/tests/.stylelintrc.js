94645c90088f416c2582dcbb38e6b2440a75fc0d
Antoine Quatrelivre <antoine@studiometa.fr>
2025-10-29 18:13:58 +0100

    Update the tests project to use the new configuration

--- packages/tests/.stylelintrc.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  extends: '@studiometa/stylelint-config/prettier',
-};
--- packages/tests/meta.config.js
+++ packages/tests/meta.config.js
@@ -1,8 +1,9 @@
 import { defineConfig } from '@studiometa/webpack-config';
+import { tailwindcss } from '@studiometa/webpack-config-preset-tailwindcss-4';
 
 export default defineConfig({
-  presets: ['tailwindcss'],
-  src: ['./src/*.scss'],
+  presets: [tailwindcss()],
+  src: ['./src/*.css'],
   dist: 'dist',
   public: '/',
   webpackProd(config) {
